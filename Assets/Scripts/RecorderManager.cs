029889bd2791ba35fc3ee5df1204b83c4c5a4246
KostasBan <mpanakak@hotmail.com>
2019-02-10 12:18:02 +0200

    Voice Recording

---
+++ Assets/Scripts/RecorderManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecorderManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RecordFile()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        //audioSource.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+        audioSource.clip = Microphone.Start(null, true, 10, 44100);
+        audioSource.Play();
+    }
+}
