1c3c111b4a5286ebdb1c68cab95bcc6cd415e851
KostasBan <mpanakak@hotmail.com>
2019-02-10 12:44:54 +0200

    Save recording in memory

--- Assets/Scripts/RecorderManager.cs
+++ Assets/Scripts/RecorderManager.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System;
 using UnityEngine;
 
 public class RecorderManager : MonoBehaviour
 {
+    private AudioSource audioSource;
+    private static int HEADER_SIZE=44;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,9 +22,19 @@ public class RecorderManager : MonoBehaviour
 
     public void RecordFile()
     {
-        AudioSource audioSource = GetComponent<AudioSource>();
         //audioSource.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+        Debug.Log(Microphone.devices);
         audioSource.clip = Microphone.Start(null, true, 10, 44100);
-        audioSource.Play();
+        StartCoroutine(StopRecording(5));
     }
+
+    public IEnumerator StopRecording(float _secs)
+    {
+        yield return new WaitForSeconds(_secs);
+        Microphone.End(null);
+        audioSource.Play();
+        SavWav.Save("myfile", audioSource.clip);
+    }
+
+  
 }
