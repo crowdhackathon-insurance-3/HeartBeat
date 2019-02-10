79908c9d99be95f2e2a5bdb8b742ae62104254ab
KostasBan <mpanakak@hotmail.com>
2019-02-10 10:39:03 +0200

    Firebase

---
+++ HeartBeat/Assets/Scripts/FireBaseManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Messaging;
+
+public class FireBaseManager : MonoBehaviour
+{
+    public Text text;
+    public void Start()
+    {
+        Debug.Log("ante");
+        FirebaseMessaging.TokenReceived += OnTokenReceived;
+        FirebaseMessaging.MessageReceived += OnMessageReceived;
+        FirebaseMessaging.SubscribeAsync("testTopic");
+    }
+
+    public void OnTokenReceived(object sender, TokenReceivedEventArgs token)
+    {
+        Debug.Log("Received Registration Token: " + token.Token);
+        text.text = token.Token;
+    }
+
+    public void OnMessageReceived(object sender, MessageReceivedEventArgs e)
+    {
+        Debug.Log("Received a new message from: " + e.Message.From);
+        Debug.Log("Received a new message from: " + e.Message.Data);
+        //Debug.Log("Received a new message from: " + e.Message.);
+
+        text.text = e.Message.Data["test"];
+    }
+}
